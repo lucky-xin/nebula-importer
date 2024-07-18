ed9401e0e6fb77524a387b67a961efa1b3be4540
luchaoxin <chaoxin.lu@pistonint.com>
2024-07-18 13:42:38 +0800

    feat(sub batch): 修改分批逻辑

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -354,7 +354,20 @@ func (m *defaultManager) loopImport(s source.Source, r reader.BatchRecordReader,
 				}
 				return nil
 			}
-			m.submitImporterTask(n, records, importers...)
+			size := len(records)
+			times := size / m.batch
+			if size%m.batch != 0 || times == 0 {
+				times++
+			}
+			for j := range times {
+				start := j * m.batch
+				end := (j + 1) * m.batch
+				if end > size {
+					end = size
+				}
+				subs := records[start:end]
+				m.submitImporterTask(n, subs, importers...)
+			}
 		}
 	}
 }
@@ -378,26 +391,14 @@ func (m *defaultManager) submitImporterTask(n int, records spec.Records, importe
 		size := len(records)
 		if size > 0 {
 			for _, i := range importers {
-				times := size / m.batch
-				if size%m.batch != 0 || times == 0 {
-					times++
-				}
-				for j := range times {
-					start := j * m.batch
-					end := (j + 1) * m.batch
-					if end > size {
-						end = size
-					}
-					subs := records[start:end]
-					result, err := i.Import(subs...)
-					if err != nil {
-						m.logError(err, "manager: import failed")
-						m.onRequestFailed(records)
-						isFailed = true
-						// do not return, continue the subsequent importer.
-					} else if result.RecordNum > 0 {
-						m.onRequestSucceeded(result)
-					}
+				result, err := i.Import(records...)
+				if err != nil {
+					m.logError(err, "manager: import failed")
+					m.onRequestFailed(records)
+					isFailed = true
+					// do not return, continue the subsequent importer.
+				} else if result.RecordNum > 0 {
+					m.onRequestSucceeded(result)
 				}
 			}
 		}
